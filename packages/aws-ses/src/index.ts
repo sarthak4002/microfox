8927d1eb4a9c052d9e671d23856fca626fb90df9
VISHWAJ33T <vishwajeety14122@gmail.com>
2025-03-17 20:38:37 +0530

    feat: Add "@microfox/aws-ses" sdk

---
+++ packages/aws-ses/src/index.ts
@@ -0,0 +1,10 @@
+export {
+  createSESSdk,
+  type SESConfig,
+  type EmailParams,
+  type BulkEmailParams,
+  type SendEmailResponse,
+  type SESSDK
+} from './aws-ses-sdk';
+
+export { default } from './aws-ses-sdk'; 
