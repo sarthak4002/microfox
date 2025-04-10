d6e8274711e9287cc88ede2bb9c92219bce57444
VISHWAJ33T <vishwajeety14122@gmail.com>
2025-04-11 03:46:56 +0530

    feat: add script for generating external package info

--- scripts/src/constants.ts
+++ scripts/src/constants.ts
@@ -1,69 +1,65 @@
+// export const SDK_QUERY = 'Google Sheets package';
+// export const API_DOCUMENTATION_URLS = [
+//   'https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets.values/get',
+//   'https://developers.google.com/workspace/sheets/api/reference/rest/v4/spreadsheets.values/batchGet',
+//   'https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets.values/update',
+//   'https://developers.google.com/workspace/sheets/api/reference/rest/v4/spreadsheets.values/batchUpdate',
+//   'https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets.values/append',
+//   'https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets.values/clear',
+//   'https://developers.google.com/workspace/sheets/api/reference/rest/v4/spreadsheets.values/batchClear',
+// ];
+
 /**
  * The query used to generate SDK
  */
-export const SDK_QUERY = 'Google Sheets package';
+export const SDK_QUERY = 'I need a brave sdk package with name "brave" and it should have functions for web-search, summarizer-search, image-search, video-search, news-search, suggest, spellcheck';
 
 /**
  * URLs for API documentation
  */
 export const API_DOCUMENTATION_URLS = [
-  'https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets.values/get',
-  'https://developers.google.com/workspace/sheets/api/reference/rest/v4/spreadsheets.values/batchGet',
-  'https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets.values/update',
-  'https://developers.google.com/workspace/sheets/api/reference/rest/v4/spreadsheets.values/batchUpdate',
-  'https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets.values/append',
-  'https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets.values/clear',
-  'https://developers.google.com/workspace/sheets/api/reference/rest/v4/spreadsheets.values/batchClear',
+  'https://api-dashboard.search.brave.com/app/documentation/web-search/get-started',
+  'https://api-dashboard.search.brave.com/app/documentation/web-search/query',
+  'https://api-dashboard.search.brave.com/app/documentation/web-search/request-headers',
+  'https://api-dashboard.search.brave.com/app/documentation/web-search/response-headers',
+  'https://api-dashboard.search.brave.com/app/documentation/web-search/responses',
+  'https://api-dashboard.search.brave.com/app/documentation/web-search/codes',
+  'https://api-dashboard.search.brave.com/app/documentation/summarizer-search/get-started',
+  'https://api-dashboard.search.brave.com/app/documentation/summarizer-search/query',
+  'https://api-dashboard.search.brave.com/app/documentation/summarizer-search/request-headers',
+  'https://api-dashboard.search.brave.com/app/documentation/summarizer-search/response-headers',
+  'https://api-dashboard.search.brave.com/app/documentation/summarizer-search/responses',
+  'https://api-dashboard.search.brave.com/app/documentation/image-search/get-started',
+  'https://api-dashboard.search.brave.com/app/documentation/image-search/query',
+  'https://api-dashboard.search.brave.com/app/documentation/image-search/request-headers',
+  'https://api-dashboard.search.brave.com/app/documentation/image-search/response-headers',
+  'https://api-dashboard.search.brave.com/app/documentation/image-search/responses',
+  'https://api-dashboard.search.brave.com/app/documentation/video-search/get-started',
+  'https://api-dashboard.search.brave.com/app/documentation/video-search/query',
+  'https://api-dashboard.search.brave.com/app/documentation/video-search/request-headers',
+  'https://api-dashboard.search.brave.com/app/documentation/video-search/response-headers',
+  'https://api-dashboard.search.brave.com/app/documentation/video-search/responses',
+  'https://api-dashboard.search.brave.com/app/documentation/news-search/get-started',
+  'https://api-dashboard.search.brave.com/app/documentation/news-search/query',
+  'https://api-dashboard.search.brave.com/app/documentation/news-search/request-headers',
+  'https://api-dashboard.search.brave.com/app/documentation/news-search/response-headers',
+  'https://api-dashboard.search.brave.com/app/documentation/news-search/responses',
+  'https://api-dashboard.search.brave.com/app/documentation/suggest/get-started',
+  'https://api-dashboard.search.brave.com/app/documentation/suggest/query',
+  'https://api-dashboard.search.brave.com/app/documentation/suggest/request-headers',
+  'https://api-dashboard.search.brave.com/app/documentation/suggest/response-headers',
+  'https://api-dashboard.search.brave.com/app/documentation/suggest/responses',
+  'https://api-dashboard.search.brave.com/app/documentation/spellcheck/get-started',
+  'https://api-dashboard.search.brave.com/app/documentation/spellcheck/query',
+  'https://api-dashboard.search.brave.com/app/documentation/spellcheck/request-headers',
+  'https://api-dashboard.search.brave.com/app/documentation/spellcheck/response-headers',
+  'https://api-dashboard.search.brave.com/app/documentation/spellcheck/responses',
 ];
 
-// /**
-//  * The query used to generate SDK
-//  */
-// export const SDK_QUERY = 'I need a brave sdk package with name "brave" and it should have functions for web-search, summarizer-search, image-search, video-search, news-search, suggest, spellcheck';
+/**
+ * GitHub URLs for external packages to generate
+ */
+export const EXT_PACKAGE_URLS = [
+  'https://github.com/slackapi/node-slack-sdk/tree/main/packages/web-api',
+];
 
-// /**
-//  * URLs for API documentation
-//  */
-// export const API_DOCUMENTATION_URLS = [
-//   'https://api-dashboard.search.brave.com/app/documentation/web-search/get-started',
-//   'https://api-dashboard.search.brave.com/app/documentation/web-search/query',
-//   'https://api-dashboard.search.brave.com/app/documentation/web-search/request-headers',
-//   'https://api-dashboard.search.brave.com/app/documentation/web-search/response-headers',
-//   'https://api-dashboard.search.brave.com/app/documentation/web-search/responses',
-//   'https://api-dashboard.search.brave.com/app/documentation/web-search/codes',
-//   'https://api-dashboard.search.brave.com/app/documentation/summarizer-search/get-started',
-//   'https://api-dashboard.search.brave.com/app/documentation/summarizer-search/query',
-//   'https://api-dashboard.search.brave.com/app/documentation/summarizer-search/request-headers',
-//   'https://api-dashboard.search.brave.com/app/documentation/summarizer-search/response-headers',
-//   'https://api-dashboard.search.brave.com/app/documentation/summarizer-search/responses',
-//   'https://api-dashboard.search.brave.com/app/documentation/image-search/get-started',
-//   'https://api-dashboard.search.brave.com/app/documentation/image-search/query',
-//   'https://api-dashboard.search.brave.com/app/documentation/image-search/request-headers',
-//   'https://api-dashboard.search.brave.com/app/documentation/image-search/response-headers',
-//   'https://api-dashboard.search.brave.com/app/documentation/image-search/responses',
-//   'https://api-dashboard.search.brave.com/app/documentation/image-search/codes',
-//   'https://api-dashboard.search.brave.com/app/documentation/video-search/get-started',
-//   'https://api-dashboard.search.brave.com/app/documentation/video-search/query',
-//   'https://api-dashboard.search.brave.com/app/documentation/video-search/request-headers',
-//   'https://api-dashboard.search.brave.com/app/documentation/video-search/response-headers',
-//   'https://api-dashboard.search.brave.com/app/documentation/video-search/responses',
-//   'https://api-dashboard.search.brave.com/app/documentation/video-search/codes',
-//   'https://api-dashboard.search.brave.com/app/documentation/news-search/get-started',
-//   'https://api-dashboard.search.brave.com/app/documentation/news-search/query',
-//   'https://api-dashboard.search.brave.com/app/documentation/news-search/request-headers',
-//   'https://api-dashboard.search.brave.com/app/documentation/news-search/response-headers',
-//   'https://api-dashboard.search.brave.com/app/documentation/news-search/responses',
-//   'https://api-dashboard.search.brave.com/app/documentation/news-search/codes',
-//   'https://api-dashboard.search.brave.com/app/documentation/suggest/get-started',
-//   'https://api-dashboard.search.brave.com/app/documentation/suggest/query',
-//   'https://api-dashboard.search.brave.com/app/documentation/suggest/request-headers',
-//   'https://api-dashboard.search.brave.com/app/documentation/suggest/response-headers',
-//   'https://api-dashboard.search.brave.com/app/documentation/suggest/responses',
-//   'https://api-dashboard.search.brave.com/app/documentation/suggest/codes',
-//   'https://api-dashboard.search.brave.com/app/documentation/spellcheck/get-started',
-//   'https://api-dashboard.search.brave.com/app/documentation/spellcheck/query',
-//   'https://api-dashboard.search.brave.com/app/documentation/spellcheck/request-headers',
-//   'https://api-dashboard.search.brave.com/app/documentation/spellcheck/response-headers',
-//   'https://api-dashboard.search.brave.com/app/documentation/spellcheck/responses',
-//   'https://api-dashboard.search.brave.com/app/documentation/spellcheck/codes',
-// ];
